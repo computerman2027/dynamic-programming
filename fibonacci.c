c2360e09283e9fe41b4fc6de4263deb534f027a2
computerman2027 <163759052+computerman2027@users.noreply.github.com>
2025-03-29 10:18:08 +0530

    fibonacci dp program upload

---
+++ fibonacci.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int fibonacci1(int termno, int *m) // using memorization technique (Top Down Approach)
+{
+    if (termno <= 1)
+        return termno;
+
+    if (m[termno] != -1)
+        return m[termno];
+
+    m[termno] = fibonacci1(termno - 1, m) + fibonacci1(termno - 2, m);
+    return m[termno];
+}
+
+int fibonacci2(int termno)
+{
+    if (termno <= 1)
+        return termno;
+
+    int *dp = (int *)malloc((termno + 1) * sizeof(int));
+    dp[0] = 0;
+    dp[1] = 1;
+    int i;
+    for (i = 2; i <= termno; i++)
+    {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[termno];
+}
+
+int main()
+{
+    int range, val, fibo1, fibo2;
+    clock_t start, end;
+    double time1, time2;
+    printf("Enter maximum range of terms : ");
+    scanf("%d", &range);
+
+    int *mem = (int *)malloc((range + 1) * sizeof(int));
+    int i;
+    for (i = 0; i < range + 1; i++)
+        mem[i] = -1;
+
+    for (i = 0; i < 4; i++)
+    {
+        printf("Enter Term no : ");
+        scanf("%d", &val);
+
+        start = clock();
+        fibo1 = fibonacci1(val - 1, mem);
+        end = clock();
+        time1 = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+        start = clock();
+        fibo2 = fibonacci2(val - 1);
+        end = clock();
+        time2 = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+        printf("Fibonacci number using func 1 for %d is %d\n", val, fibo1);
+        printf("Fibonacci number using func 2 for %d is %d\n", val, fibo2);
+        printf("Time required for func 1 is %lf s and func 2 is %lf s\n", time1, time2);
+    }
+
+    return 0;
+}
